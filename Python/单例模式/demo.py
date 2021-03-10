3e391b3cd45eca42654fcb75b44c912c9be118d8
水月 <sun.melodies@gmail.com>
2021-03-10 17:43:52 +0800

    Python 单例模式 demo

---
+++ Python/单例模式/demo.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import time
+import random
+
+
+
+def do_something():
+    """
+    我是一个非常消耗资源的函数
+    """
+    s = random.randint(1, 5)
+    time.sleep(s)
+    return s
+
+
+def get(refresh=False):
+
+    if refresh:
+        get.resource = ""
+
+    if get.resource:
+        return get.resource
+
+    get.resource = do_something()
+
+    return get.resource
+
+
+# 把全局状态放到私有变量中
+get.resource = {}
+
+
+def main():
+    print(get())
+
+
+if __name__ == "__main__":
+    main()
