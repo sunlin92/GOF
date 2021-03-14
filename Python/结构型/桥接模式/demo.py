c1556cc1b0a5b2ff348cb43146c5e744f749403e
水月 <sun.melodies@gmail.com>
2021-03-14 11:50:51 +0800

    fix:Python 桥接模式 demo

--- Python/结构型/桥接模式/demo.py
+++ Python/结构型/桥接模式/demo.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+import collections
+import abc
 
 
 def has_methods(*methods):
     def decorator(Base):
         def __subclasshook__(cls, Subclass):
             if cls is Base:
-                import collections
-
                 attributes = collections.ChainMap(
                     *(Superclass.__dict__ for Superclass in Subclass.__mro__)
                 )
@@ -23,25 +23,33 @@ def has_methods(*methods):
 
 # 抽象软件类
 @has_methods("run")
-class HandsetSoft(object):
+class App(metaclass=abc.ABCMeta):
     pass
 
 
 # 手游
-class HandsetGame(HandsetSoft):
+class Game(App):
     def run(self):
         print("运行手机游戏")
 
 
 # 通讯录
-class HandsetAddressList(HandsetSoft):
+class AddressList(App):
     def run(self):
         print("运行通信录")
 
 
 # 抽象手机品牌类
 @has_methods("runapp")
-class Phone(object):
+class Phone(metaclass=abc.ABCMeta):
+    def set_handle_app(self, app):
+        if not isinstance(app, App):
+            raise TypeError(
+                "Expected object of type App, got {}".format(
+                    type(app).__name__
+                )
+            )
+        self.app = app
     pass
 
 
@@ -49,28 +57,32 @@ class GooglePixel(Phone):
     def do_something_for_android(self):
         print("google pixel is doing something")
 
-    def runapp(self, app):
+    def runapp(self):
         self.do_something_for_android()
-        app.run()
+        self.app.run()
 
 
 class AppleIphone(Phone):
     def do_something_for_ios(self):
         print("apple iphone is doing something")
 
-    def runapp(self, app):
+    def runapp(self):
         self.do_something_for_ios()
-        app.run()
+        self.app.run()
 
 
 def main():
     pixel5 = GooglePixel()
-    pixel5.runapp(HandsetGame())
-    pixel5.runapp(HandsetAddressList())
+    pixel5.set_handle_app(Game())
+    pixel5.runapp()
+    pixel5.set_handle_app(AddressList())
+    pixel5.runapp()
 
     iphone12 = AppleIphone()
-    iphone12.runapp(HandsetGame())
-    iphone12.runapp(HandsetAddressList())
+    iphone12.set_handle_app(Game())
+    iphone12.runapp()
+    iphone12.set_handle_app(AddressList())
+    iphone12.runapp()
 
 
 if __name__ == "__main__":
